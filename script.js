ccf24f2a3a297ab8f5cf328f235e3926d9254fe3
Antonio Macaringue <110739274+Makas-M@users.noreply.github.com>
2023-02-12 11:15:00 +0200

    adicao da lista de videos

--- script.js
+++ script.js
@@ -13,3 +13,16 @@ function videoDois(){
 	titulo.innerHTML = 'Remake Mlindo - Amakhaya  - Dj Makas - Fl Studio';
 	video.innerHTML = '<iframe width="560" height="315" src="https://www.youtube.com/embed/2MnGMr6GhBc" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>';
 }
+function videoTres(){
+	var titulo = document.querySelector('#titulo');
+	var video = document.querySelector('#video1');
+
+	titulo.innerHTML = 'Remake Bander & Dygo - Minha Shuga  - Dj Makas - Fl Studio';
+	video.innerHTML = '<iframe width="560" height="315" src="https://www.youtube.com/embed/wXEw06jHsF8" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>'; 
+}
+function videoQuatro(){
+	var titulo = document.querySelector('#titulo');
+	var video = document.querySelector('#video1');
+
+	titulo.innerHTML = 'Remake Eamon - I dont Want you back - Dj Makas - Fl Studio';
+	video.innerHTML = '<iframe width="560" height="315" src="https://www.youtube.com/embed/ti-tviDXbws" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>';}
