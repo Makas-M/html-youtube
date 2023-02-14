c803bbbf7fdbc31b827a90d5d7b7597326582a25
Antonio Macaringue <110739274+Makas-M@users.noreply.github.com>
2023-02-14 20:35:31 +0200

    lista

--- js/script.js
+++ js/script.js
@@ -18,11 +18,11 @@ function videoTres(){
 	var video = document.querySelector('#video1');
 
 	titulo.innerHTML = 'Remake Bander & Dygo - Minha Shuga  - Dj Makas - Fl Studio';
-	video.innerHTML = '<iframe width="560" height="315" src="https://www.youtube.com/embed/wXEw06jHsF8" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>'; 
-}
+	video.innerHTML = '<iframe width="560" height="315" src="https://www.youtube.com/embed/wXEw06jHsF8" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>';}
 function videoQuatro(){
 	var titulo = document.querySelector('#titulo');
 	var video = document.querySelector('#video1');
 
 	titulo.innerHTML = 'Remake Eamon - I dont Want you back - Dj Makas - Fl Studio';
-	video.innerHTML = '<iframe width="560" height="315" src="https://www.youtube.com/embed/ti-tviDXbws" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>';}
+	video.innerHTML = '<iframe width="560" height="315" src="https://www.youtube.com/embed/ti-tviDXbws" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>';
+}
